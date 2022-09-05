0ea943fe314a3619801e9284a6f4d56405195dac
OVYVO <vue12306@163.com>
2022-09-05 14:03:16 +0800

    fix: 自动部署测试

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,6 +2,7 @@ const path = require('path')
 const components = require('./components.js') 
 
 module.exports={
+  base:'/vue-utils/',  
   title: 'Rocket-UI',
   head: [
     ['link',{ rel:'icon',href: '/rocket.png'}]
