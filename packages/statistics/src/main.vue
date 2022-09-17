99a523eee9712a56f27436dc79868de879eabd05
OVYVO <vue12306@163.com>
2022-09-17 18:58:39 +0800

    fix: 文档更新

--- packages/statistics/src/main.vue
+++ packages/statistics/src/main.vue
@@ -122,12 +122,4 @@ export default {
   line-height: 24px;
 }
 </style>
-<style>
-.icon {
-  width: 1em;
-  height: 1em;
-  vertical-align: -0.15em;
-  fill: currentColor;
-  overflow: hidden;
-}
-</style>
+
